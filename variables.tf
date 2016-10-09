ffac619f5b507a1d42a935a79c249994eda8192a
James Turnbull <james@lovedthanlost.net>
2016-10-09 13:51:20 -0400

    Updated variables

--- variables.tf
+++ variables.tf
@@ -1,20 +1,16 @@
 variable "name" {}
-
 variable "cidr" {}
-
 variable "public_subnet" {}
 
 variable "enable_dns_hostnames" {
-  description = "should be true if you want to use private DNS within the VPC"
-  default     = false
+  description = "Should be true if you want to use private DNS within the VPC"
+  default     = true
 }
-
 variable "enable_dns_support" {
-  description = "should be true if you want to use private DNS within the VPC"
-  default     = false
+  description = "Should be true if you want to use private DNS within the VPC"
+  default     = true
 }
-
 variable "map_public_ip_on_launch" {
-  description = "should be false if you do not want to auto-assign public IP on launch"
+  description = "Should be false if you do not want to auto-assign public IP on launch"
   default     = true
 }
