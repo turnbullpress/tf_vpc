4957ef30daaf6e5767a4451b5f4779ca40de4ee8
James Turnbull <james@lovedthanlost.net>
2016-10-19 11:50:59 +1100

    Added variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,21 +1,50 @@
-variable "environment" {}
-
 variable "region" {
-  default = "us-east-1"
+  description = "The AWS region."
 }
 
-variable "vpc_cidr" {}
+variable "environment" {
+  description = "The name of our environment, i.e. development."
+}
+
+variable "key_name" {
+  description = "The AWS key pair to use for resources."
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR of the VPC."
+}
 
 variable "public_subnets" {
-  default = []
+  default     = []
+  description = "The list of public subnets to populate."
 }
 
 variable "private_subnets" {
-  default = []
+  default     = []
+  description = "The list of private subnets to populate."
+}
+
+variable "ami" {
+  default = {
+    "us-east-1" = "ami-f652979b"
+    "us-west-1" = "ami-7c4b331c"
+  }
+
+  description = "The AMIs to use for web and app instances."
+}
+
+variable "instance_type" {
+  default     = "t2.micro"
+  description = "The instance type to launch "
+}
+
+variable "vpc_id" {
+  description = "The VPC ID to launch in"
 }
 
 variable "bastion_instance_type" {
-  default = "t2.micro"
+  default     = "t2.micro"
+  description = "The bastion host instance type."
 }
 
 variable "bastion_ami" {
@@ -23,9 +52,9 @@ variable "bastion_ami" {
     "us-east-1" = "ami-f652979b"
     "us-west-1" = "ami-7c4b331c"
   }
-}
 
-variable "key_name" {}
+  description = "The bastion host AMIs."
+}
 
 variable "enable_dns_hostnames" {
   description = "Should be true if you want to use private DNS within the VPC"
