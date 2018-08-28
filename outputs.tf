aa4d8bbf523db0222ad182ebdaed3ce06829c7d6
James Turnbull <james@lovedthanlost.net>
2018-08-28 10:54:47 -0400

    Fixed output

--- outputs.tf
+++ outputs.tf
@@ -27,7 +27,7 @@ output "public_route_table_id" {
 }
 
 output "private_route_table_id" {
-  value = ["${aws_route_table.private.id}"]
+  value = ["${aws_route_table.private.*.id}"]
 }
 
 output "default_security_group_id" {
