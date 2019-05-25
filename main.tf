55ea5b5d5cbfb7cb5bd0fbbe4a0eff947f8e855a
James Turnbull <james@lovedthanlost.net>
2019-05-25 14:32:14 -0400

    Upgraded to Terraform 0.12

--- main.tf
+++ main.tf
@@ -1,102 +1,102 @@
 resource "aws_vpc" "environment" {
-  cidr_block           = "${var.vpc_cidr}"
-  enable_dns_hostnames = "${var.enable_dns_hostnames}"
-  enable_dns_support   = "${var.enable_dns_support}"
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
 
-  tags {
-    Name = "${var.environment}"
+  tags = {
+    Name = var.environment
   }
 }
 
 resource "aws_internet_gateway" "environment" {
-  vpc_id = "${aws_vpc.environment.id}"
+  vpc_id = aws_vpc.environment.id
 
-  tags {
+  tags = {
     Name = "${var.environment}-igw"
   }
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.environment.id}"
+  vpc_id = aws_vpc.environment.id
 
-  tags {
+  tags = {
     Name = "${var.environment}-public"
   }
 }
 
 resource "aws_route_table" "private" {
-  count  = "${length(var.private_subnets)}"
-  vpc_id = "${aws_vpc.environment.id}"
+  count  = length(var.private_subnets)
+  vpc_id = aws_vpc.environment.id
 
-  tags {
+  tags = {
     Name = "${var.environment}-private"
   }
 }
 
 resource "aws_route" "public_internet_gateway" {
-  route_table_id         = "${aws_route_table.public.id}"
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.environment.id}"
+  gateway_id             = aws_internet_gateway.environment.id
 }
 
 resource "aws_route" "private_nat_gateway" {
-  count                  = "${length(var.private_subnets)}"
-  route_table_id         = "${aws_route_table.private.*.id[count.index]}"
+  count                  = length(var.private_subnets)
+  route_table_id         = aws_route_table.private[count.index].id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.environment.*.id[count.index]}"
+  nat_gateway_id         = aws_nat_gateway.environment[count.index].id
 }
 
 resource "aws_subnet" "public" {
-  vpc_id                  = "${aws_vpc.environment.id}"
-  cidr_block              = "${var.public_subnets[count.index]}"
-  map_public_ip_on_launch = "${var.map_public_ip_on_launch}"
+  vpc_id                  = aws_vpc.environment.id
+  cidr_block              = var.public_subnets[count.index]
+  map_public_ip_on_launch = var.map_public_ip_on_launch
 
-  tags {
+  tags = {
     Name = "${var.environment}-public-${count.index}"
   }
 
-  count = "${length(var.public_subnets)}"
+  count = length(var.public_subnets)
 }
 
 resource "aws_subnet" "private" {
-  vpc_id                  = "${aws_vpc.environment.id}"
-  cidr_block              = "${var.private_subnets[count.index]}"
+  vpc_id                  = aws_vpc.environment.id
+  cidr_block              = var.private_subnets[count.index]
   map_public_ip_on_launch = "false"
 
-  tags {
+  tags = {
     Name = "${var.environment}-private-${count.index}"
   }
 
-  count = "${length(var.private_subnets)}"
+  count = length(var.private_subnets)
 }
 
 resource "aws_route_table_association" "private" {
-  count          = "${length(var.private_subnets)}"
-  subnet_id      = "${aws_subnet.private.*.id[count.index]}"
-  route_table_id = "${aws_route_table.private.*.id[count.index]}"
+  count          = length(var.private_subnets)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
 
 resource "aws_route_table_association" "public" {
-  count          = "${length(var.public_subnets)}"
-  subnet_id      = "${aws_subnet.public.*.id[count.index]}"
-  route_table_id = "${aws_route_table.public.id}"
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_eip" "environment" {
-  count = "${length(var.public_subnets)}"
+  count = length(var.public_subnets)
 
   vpc = true
 }
 
 resource "aws_nat_gateway" "environment" {
-  count = "${length(var.public_subnets)}"
+  count = length(var.public_subnets)
 
-  allocation_id = "${aws_eip.environment.*.id[count.index]}"
-  subnet_id     = "${aws_subnet.public.*.id[count.index]}"
+  allocation_id = aws_eip.environment[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
 }
 
 resource "aws_security_group" "bastion" {
-  vpc_id      = "${aws_vpc.environment.id}"
+  vpc_id      = aws_vpc.environment.id
   name        = "${var.environment}-bastion-host"
   description = "Allow SSH to bastion host"
 
@@ -121,21 +121,22 @@ resource "aws_security_group" "bastion" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags {
+  tags = {
     Name = "${var.environment}-bastion-sg"
   }
 }
 
 resource "aws_instance" "bastion" {
-  ami                         = "${lookup(var.bastion_ami, var.region)}"
-  instance_type               = "${var.bastion_instance_type}"
-  key_name                    = "${var.key_name}"
+  ami                         = var.bastion_ami[var.region]
+  instance_type               = var.bastion_instance_type
+  key_name                    = var.key_name
   monitoring                  = true
-  vpc_security_group_ids      = ["${aws_security_group.bastion.id}"]
-  subnet_id                   = "${aws_subnet.public.*.id[0]}"
+  vpc_security_group_ids      = [aws_security_group.bastion.id]
+  subnet_id                   = aws_subnet.public[0].id
   associate_public_ip_address = true
 
-  tags {
+  tags = {
     Name = "${var.environment}-bastion"
   }
 }
+
